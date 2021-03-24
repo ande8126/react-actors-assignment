4ff7e63b81f97f28fb2f17d4c18cb55fb84d3cfc
Pat Anderson <patrick.b.anderson@gmail.com>
2021-03-24 00:41:01 -0500

    got event.target.value function working

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,10 +5,13 @@ import About from '../About/About';
 import Contact from '../Contact/Contact';
 import Footer from '../Footer/Footer';
 
+
 class App extends Component {
+
   render() {
     return (
       <div className="App">
+
         <Header />
         <About />
         <Contact />
--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,7 +1,19 @@
+import  { useState } from 'react';
+
+
 function Contact(){
+    const [ words, setWords ] = useState( '1' )
+
+    let handleInput = (event=>{
+        console.log( 'in handleInput', event.target.value );
+        setWords(event.target.value);
+    })
+    
     return(
         <div>
             <h2>Contact</h2>
+            <input onChange={handleInput} type="text" placeholder="Actor name here"></input>
+            <p>{words}</p>
             <p>Phone: 1-800-WE-REACT</p>
             <p>Email: info@ReactActors.com</p>
         </div>
