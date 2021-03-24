82a1ea02d5332bf4fbea2f2cc7590182ce4c08cf
Pat Anderson <patrick.b.anderson@gmail.com>
2021-03-24 10:49:54 -0500

    added functions for first and last name

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -2,18 +2,31 @@ import  { useState } from 'react';
 
 
 function Contact(){
-    const [ words, setWords ] = useState( '1' )
+    const [ words, setWords ] = useState( '' );
+    const [ fName, setFName ] = useState( '' );
+    const [ lName, setLName ] = useState( '' );
 
-    let handleInput = (event=>{
+    let handleFirst = ( name ) =>{
+        setFName( name.target.value );
+    }
+
+    let handleLast = ( name ) =>{
+        setLName( name.target.value );
+    }
+
+    let handleImpression = ( event ) =>{
         console.log( 'in handleInput', event.target.value );
-        setWords(event.target.value);
-    })
+        setWords( event.target.value );
+    }
+
     
     return(
         <div>
             <h2>Contact</h2>
-            <input onChange={handleInput} type="text" placeholder="Actor name here"></input>
-            <p>{words}</p>
+            <input onChange={handleFirst} type="text" placeholder="First name"></input>
+            <input onChange={handleLast} type="text" placeholder="Last name"></input>
+            <input onChange={handleImpression} type="text" placeholder="Best impression"></input>
+            <p>{fName} {lName} {words}</p>
             <p>Phone: 1-800-WE-REACT</p>
             <p>Email: info@ReactActors.com</p>
         </div>
